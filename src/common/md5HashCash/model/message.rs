abaa1969f67b22582dbb2e4dc53c8a84964bb914
Meziane270 <moulmas@myges.fr>
2022-07-07 19:39:41 +0200

    adding enities

---
+++ src/common/md5HashCash/model/message.rs
@@ -0,0 +1,10 @@
+pub enum Message {
+    Welcome(Welcome),
+    Subscribe(Subscribe),
+    SubscribeResult(SubscribeResult),
+    PublicLeaderBoard(Vec<PublicPlayer>),
+    Challenge(Challenge),
+    ChallengeResult(ChallengeResult),
+    RoundSummary(RoundSummary),
+    EndOfGame(EndOfGame),
+}
